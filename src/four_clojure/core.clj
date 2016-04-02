413d5f83ad301ff4be7e5b14bd4af055eab20312
olli <ollikarppinen@gmail.com>
2016-04-02 19:30:35 +0300

    30 done

--- src/four_clojure/core.clj
+++ src/four_clojure/core.clj
@@ -9,6 +9,7 @@
   27 "(fn isPalindrome [s] (if (< (count s) 2)  true (if (= (first s) (last s)) (isPalindrome (drop 1 (drop-last s))) false)))"
   28 "(fn f [x] (mapcat #(if (coll? %) (f %) (list %)) x))"
   29 "(fn [x] (apply str (filter #(Character/isUpperCase %) x)))"
+  30 "(fn f [x] (cons (first x) (map last (filter #(not= (first %) (last %)) (partition 2 1 x)))))"
   32 "mapcat #(vector % %)"
   ))
   (println "\n === Solutions for 4Clojure problems: === \n")
