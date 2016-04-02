533ec3c900dd19cc5dcb09c44d4f903281341c78
olli <ollikarppinen@gmail.com>
2016-04-02 17:18:07 +0300

    Solution printing now in numerical order

--- src/four_clojure/core.clj
+++ src/four_clojure/core.clj
@@ -4,11 +4,11 @@
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (def solutions (hash-map
+  (def solutions (sorted-map
   26 "#(take % (map last (iterate (fn [x] (cons (last x) (list (reduce + x)))) '(0 1))))"
   27 "(fn isPalindrome [s] (if (< (count s) 2)  true (if (= (first s) (last s)) (isPalindrome (drop 1 (drop-last s))) false)))"
   28 "(fn f [x] (mapcat #(if (coll? %) (f %) (list %)) x))"
   ))
   (println "\n === Solutions for 4Clojure problems: === \n")
-  (println (reverse (map #(str "\n" (key %) ": " (solutions (key %)) "\n") solutions)))
+  (println (map #(str "\n" (key %) ": " (solutions (key %)) "\n") solutions))
   (println "\n ===   (>_<)   ===\n"))
