344c96c7b6d3623e91a900d5b721d6f55fa7594c
olli <ollikarppinen@gmail.com>
2016-04-02 15:33:04 +0300

    28 solved

--- src/four_clojure/core.clj
+++ src/four_clojure/core.clj
@@ -7,7 +7,7 @@
   (def solutions (hash-map
   26 "#(take % (map last (iterate (fn [x] (cons (last x) (list (reduce + x)))) '(0 1))))"
   27 "(fn isPalindrome [s] (if (< (count s) 2)  true (if (= (first s) (last s)) (isPalindrome (drop 1 (drop-last s))) false)))"
-  28 "asd"
+  28 "(fn f [x] (mapcat #(if (coll? %) (f %) (list %)) x))"
   ))
   (println "\n === Solutions for 4Clojure problems: === \n")
   (println (reverse (map #(str "\n" (key %) ": " (solutions (key %)) "\n") solutions)))
