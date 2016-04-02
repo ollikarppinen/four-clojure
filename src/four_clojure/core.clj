3437f533a14015b92bd32930270bd67fc67c4c6c
olli <ollikarppinen@gmail.com>
2016-04-02 13:34:21 +0300

    solutions for problem 27 added

--- src/four_clojure/core.clj
+++ src/four_clojure/core.clj
@@ -6,7 +6,7 @@
   [& args]
   (def solutions (hash-map
   26 "#(take % (map last (iterate (fn [x] (cons (last x) (list (reduce + x)))) '(0 1))))"
-  27 "asd"
+  27 "(fn isPalindrome [s] (if (< (count s) 2)  true (if (= (first s) (last s)) (isPalindrome (drop 1 (drop-last s))) false)))"
   28 "asd"
   ))
   (println "\n === Solutions for 4Clojure problems: === \n")
