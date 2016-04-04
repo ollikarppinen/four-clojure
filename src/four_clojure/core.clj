232fac83597018da482f254192f15059c31b1010
olli <ollikarppinen@gmail.com>
2016-04-04 14:57:18 +0300

    39 and 40 solutions

--- src/four_clojure/core.clj
+++ src/four_clojure/core.clj
@@ -15,7 +15,9 @@
   33 "(fn [x, y] (mapcat #(repeat y %) x))"
   34 "#(take (- %2 %1) (iterate inc %1))"
   38 "(fn [& x] (reduce #(if (< %1 %2) %2 %1) x))"
+  39 "(fn f [x y] (mapcat #(list (nth x %) (nth y %)) (range (min (count x) (count y)))))"
+  40 "#(drop 1 (mapcat (fn [x] (list %1 x)) %2))"
   ))
   (println "\n === Solutions for 4Clojure problems: === \n")
   (println (map #(str "\n" (key %) ": " (solutions (key %)) "\n") solutions))
-  (println "\n ===   (>_<)   ===\n"))
+  (println "\n ======\n"))
